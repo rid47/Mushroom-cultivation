bec25eb003b6595d892b60f54854146acdd82296
rid47 <ridwanmizan@gmail.com>
2018-08-06 15:08:23 +0600

    Add files via upload
    Library : https://github.com/beegee-tokyo/DHTesp

---
+++ Code optimization/reading_dht22_using_ESP32.ino
@@ -0,0 +1,24 @@
+#include "DHTesp.h"
+ 
+DHTesp dht;
+ 
+void setup()
+{
+  Serial.begin(115200);
+ 
+  dht.setup(26,DHTesp::DHT22);
+}
+ 
+void loop()
+{
+  float temperature = dht.getTemperature();
+ 
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+
+  int humidity=dht.getHumidity();
+  Serial.print("Humidity:");
+  Serial.println(humidity);
+  delay(10000);
+ 
+}
